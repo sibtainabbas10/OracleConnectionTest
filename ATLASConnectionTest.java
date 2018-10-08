9c5a5dbdbc1dea8dbf96baa02e1a14907a1a5bee
sibtainabbas10 <sibtain.abbas10@gmail.com>
2018-10-08 16:12:43 +0500

    Update ATLASConnectionTest.java

--- ATLASConnectionTest.java
+++ ATLASConnectionTest.java
@@ -2,7 +2,7 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class ATLASConnTest {
+public class ATLASConnectionTest {
 
     public static void main(String[] argv) {
 
